a2696b94ac81b67714263ebb2368be863a33454f
Guimunaretto <62663964+Guimunaretto@users.noreply.github.com>
2021-04-03 14:19:11 -0300

    Update dice_roller.py
    Now rolling 2 dice

--- dice_roller.py
+++ dice_roller.py
@@ -1,6 +1,10 @@
 def main():
-  
-  roll = 5
- print(f'You rolled a {roll}')
+ import random
+ dice_sum = 0
+ dice_rolls = 2
+ for i in range(0,dice_rolls):    
+ 	 roll = random.randint(1,6)
+ 	 dice_sum += rolled
+ print (f'You have rolled a sum of {dice_sum}') 
 if __name__== "__main__":
   main()
